6c63baa05379f655c5d31282c81c58a3a1045503
xymabinogi <edelweisshong@gmail.com>
2018-09-29 11:37:53 -0400

    more motiv

--- plot_motivation.m
+++ plot_motivation.m
@@ -2,7 +2,7 @@ addpath('matlab_func');
 common_settings;
 barWidth = 0.5;
 figureSize = figSizeThreeFourth;
-plots  = [true, true, true, true, false];
+plots  = [0, 0, 0, 0, 0,true ];
 
 machines = {'G1','G2','C3', 'C4'};
 if plots(1)    
@@ -87,6 +87,57 @@ if plots(4)
     % set(gca,'YTickLabel', machines,'FontSize',fontAxis);
     fileNames{figIdx} = 'alg_ex2';
 end
+
+if plots(5)    
+       temp = [0.0 0 15/4 6/4];
+   figIdx=figIdx +1;         
+   figures{figIdx} = figure;
+   scrsz = get(groot, 'ScreenSize');      
+    hold on   
+        h(1)= fill([4 4 0 0],[0.5 1.5 1.5 0.5],colorUser2);
+       h(2)= fill([9 9 4 4],[0.5 1.5 1.5 0.5],colorUser3);
+        h(3)= fill([4 4 0 0],[1.5 2.5 2.5 1.5],colorUser1);
+     %   h(4)= fill([10 10 0 0],[3.5 4.5 4.5 3.5],colorUser4);
+        
+        legend([h(1) h(2) h(3)],{'Job 2', 'Job 3','Job 1'},'Location','northeast')
+    hold off
+     xlim([0 10]);
+     ylim([0.5 2.5]);
+    set (gcf, 'Units', 'Inches', 'Position', temp, 'PaperUnits', 'inches', 'PaperPosition', temp);
+%     xlabel(xLabel,'FontSize',fontAxis);
+    set(gca,'YTickLabel', machines,'FontSize',fontAxis);
+    fileNames{figIdx} = 'FS_ex';
+end
+
+if plots(6)   
+     temp = [0.0 0 15/4 6/4];
+   figIdx=figIdx +1;         
+   figures{figIdx} = figure;
+   scrsz = get(groot, 'ScreenSize');      
+    hold on   
+        h(1)= fill([10 10 0 0],[0.5 1.5 1.5 0.5],colorUser1);
+         h(2) = rectangle('Position',[10 0.5 5 1],'LineStyle',':');
+         plot(4,0.5,'r*')
+         plot(7,0.5,'r*')
+         plot(10,0.5,'r*')
+         plot(15,0.5,'r*')
+         text(3.3,0.2,'$$a_{j}=4$$','Interpreter','latex')
+          text(6.7,0.2,'$$T$$','Interpreter','latex')
+          text(9.3,0.2,'$$\omega_{i} = 10$$','Interpreter','latex')
+          text(14.3,0.2,'$$c_{j} = 15$$','Interpreter','latex')
+         axis off
+    hold off
+     xlim([0 18]);
+     ylim([-0.5 1.5]);
+    set (gcf, 'Units', 'Inches', 'Position', temp, 'PaperUnits', 'inches', 'PaperPosition', temp);
+%     xlabel(xLabel,'FontSize',fontAxis);
+    set(gca,'YTickLabel', []);
+    set(gca,'xtick',[]);
+    fileNames{figIdx} = 'arrival_ex';
+end
+
+
+
 
 
 for i=1:length(fileNames)
